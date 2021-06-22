d2d7a56d3638a6e7f836338729f239d01c7fbd6e
TheGamerH18 <44879628+TheGamerH18@users.noreply.github.com>
2021-06-22 20:35:07 +0200

    Initial Commit

---
+++ info.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <link rel="stylesheet" href="style.css">
+    <meta http-equiv="refresh" content="10">
+    <meta charset="utf-8">
+    <script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script>
+        
+      window.onload = function() {
+        var datapoints1 = <?php
+          $str = file_get_contents("data2021-06-19.json");
+          $info = json_decode($str, true);
+
+          for ($x = 0; $x <= count($info)-1; $x ++) {
+            $final[$x]["x"] = round($info[$x]["time"]);
+            $final[$x]["y"] = round($info[$x]["temperatur"]);
+          }
+          echo json_encode($final, JSON_NUMERIC_CHECK);
+          ?>;
+        var interval = 5000
+
+        var chart = new CanvasJS.Chart("chartContainer", {
+          zoomEnabled: true,
+          theme: "dark1",
+          title: {
+            text: "Temperatur"
+          },
+          axisX: {
+            title: "Chart updates every some seconds"
+          },
+          axisY: {
+            suffix: " °C"
+          },
+          toolTip: {
+            shared: true
+          },
+          legend: {
+            cursor: "pointer",
+            verticalAlign: "top",
+            fontSize: 22,
+            fontColor: "dimGrey",
+            itemclick: toggleDataSeries
+          },
+          data: [{
+            type: "line",
+            name: "Temperatur",
+            xValueType: "dateTime",
+            yValueFormatString: "# °C",
+            xValueFormatString: "hh:mm:ss",
+            showInLegend: true,
+            legendText: "{name}",
+            dataPoints: datapoints1
+          }]
+        });
+
+				chart.render();
+
+        function toggleDataSeries(e) {
+          if(typeof(e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
+            e.dataSeries.visible = false;
+          } else {
+            e.dataSeries.visible = true;
+          }
+          chart.render();
+        }
+        
+      }
+    </script>
+    <title></title>
+  </head>
+  <body><?php
+      echo $final[count($final) - 1]["y"]
+      ?> °C
+    <div id="chartContainer">
+
+    </div>
+  </body>
+</html>
---
+++ main.py
@@ -0,0 +1,55 @@
+import json
+import time
+from datetime import datetime
+
+import RPi.GPIO as GPIO
+import dht11
+
+# Initialize GPIO pins
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.cleanup()
+
+# Set sensor Object
+sensor = dht11.DHT11(pin=4)
+
+temperatur = ""
+humidity = ""
+
+
+def main():
+    while True:
+        getvalues()
+        time.sleep(5)
+
+
+def getvalues():
+    global temperatur, humidity
+    result = sensor.read()
+    if result.is_valid():
+        temperatur = result.temperature
+        humidity = result.humidity
+        print("Temperatur: %-3.1f C" % result.temperature)
+        print("Humidity: %-3.1f %%" % result.humidity)
+        exportvalues()
+    else:
+        print("Error: %d" % result.error_code)
+
+
+def exportvalues():
+    currentdate = datetime.now().strftime("%Y-%m-%d")
+    try:
+        data = json.load(open("/var/www/html/data{0}.json".format(currentdate)))
+    except OSError:
+        data = [
+
+        ]
+    except:
+        print("Error in JSON")
+    currenttime = datetime.now().timestamp() * 1000
+    data.append({"time": currenttime, "temperatur": temperatur, "humidity": humidity})
+    json.dump(data, open("/var/www/html/data{0}.json".format(currentdate), "w"), indent=4)
+
+
+if __name__ == "__main__":
+    main()
