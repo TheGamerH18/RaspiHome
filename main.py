ac3663d0e2d3557dc5a98ff3ab9c03f7b67d696e
TheGamerH18 <44879628+TheGamerH18@users.noreply.github.com>
2021-06-28 00:35:34 +0200

    Add 7 Segment Display as Clock

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os.path
 import sys
 import time
 from datetime import datetime
+from threading import Thread
 
 import RPi.GPIO as GPIO
 import dht11
@@ -20,11 +21,42 @@ temperatur = ""
 humidity = ""
 path = os.path.dirname(os.path.abspath(sys.argv[0]))
 
+# Data for 7 segment display
+# Pins of Segments
+segments =  [6, 11, 16, 20, 21, 5, 19, 26]
+# Pins of Digits
+digits = [12, 7, 8, 13]
+# Data to display numbers
+num = {' ':[1,1,1,1,1,1,1],
+    '0':[0,0,0,0,0,0,1],
+    '1':[1,0,0,1,1,1,1],
+    '2':[0,0,1,0,0,1,0],
+    '3':[0,0,0,0,1,1,0],
+    '4':[1,0,0,1,1,0,0],
+    '5':[0,1,0,0,1,0,0],
+    '6':[0,1,0,0,0,0,0],
+    '7':[0,0,0,1,1,1,1],
+    '8':[0,0,0,0,0,0,0],
+    '9':[0,0,0,0,1,0,0]}
+# Init digits
+for digit in digits:
+    GPIO.setup(digit, GPIO.OUT)
+    GPIO.output(digit, 0)
+# Init segments
+for segment in segments:
+    GPIO.setup(segment, GPIO.OUT)
+    GPIO.output(segment, 1)
 
 def main():
-    while True:
-        getvalues()
-        time.sleep(30)
+    try:
+        # Create Thread for Clock
+        th = Thread(target=currenttime)
+        th.start()
+        while True:
+            getvalues()
+            time.sleep(30)
+    finally:
+        GPIO.cleanup()
 
 
 def getvalues():
@@ -59,6 +91,21 @@ def exportvalues():
     # Dump to file
     json.dump(data, open("{0}/web/data/{1}.json".format(path, currentdate), "w"), indent=4)
 
+def currenttime():
+    while True:
+        # Get current time
+        dtstring = datetime.now().strftime("%H%M")
+        # Loop through dtstring to get specific Digits
+        for index in range(0, len(dtstring)):
+            # Select digit
+            GPIO.output(digits[index], 1)
+            # Show number on Digit
+            for integ in range(0, 7):
+                # Selects the Segmen 0-7, defines if it is on or off
+                GPIO.output(segments[integ], num[dtstring[index]][integ])
+            time.sleep(0.002)
+            # Close Digit
+            GPIO.output(digits[index], 0)
 
 if __name__ == "__main__":
     main()
